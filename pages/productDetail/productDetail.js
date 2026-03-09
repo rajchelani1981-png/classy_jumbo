22e61928f9dbd87196985dd6ce44dfa8602b5e75
sunnylalwani41 <sunnylalwani41@gmail.com>
2026-03-09 19:18:20 +0530

    product added

--- pages/productDetail/productDetail.js
+++ pages/productDetail/productDetail.js
@@ -367,7 +367,7 @@ let loadTheProductDetailData = async () => {
           <ul>
             <li><strong>Contains:</strong> ${e.item_package_material} of ${product.flavour} ${product.category == product.flavour ? "" : product.category}</li>
             <li><strong>Packaging:</strong> ${e.item_package_material} with outer ${e.box_material}</li>
-            <li><strong>Carton Packaging:</strong> ${e.box_contains} ${e.item_package_material} × ${e.item_package_contain != 1 ? `${e.item_package_contain} ${e.item_count_type} × ` : "" } ${product.net_weight ? product.net_weight : e.product_weight} ${e.product_mrp ? `(${e.product_mrp} ₹ / ${e.item_count_type})` : "" }</li>
+            <li><strong>Carton Packaging:</strong> ${e.box_contains} ${e.item_package_material} × ${e.item_package_contain != 1 && e.item_package_contain ? `${e.item_package_contain} ${e.item_count_type} × ` : "" } ${product.net_weight ? product.net_weight : e.product_weight} ${e.product_mrp ? `(${e.product_mrp} ₹ / ${e.item_count_type})` : "" }</li>
             <li><strong>Per Piece Weight:</strong> ${product.net_weight ? product.net_weight : e.product_weight}</li>
           </ul>
         </div>
